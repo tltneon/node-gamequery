1f1625c200c0480b5229c11f75160c21808e2d9f
Kurt Stolle <kurt@casualbananas.com>
2017-03-17 12:49:08 +0100

    Very basic testing setup

---
+++ test/index.js
@@ -0,0 +1,25 @@
+const vows = require("vows");
+const assert = require("assert");
+const gamequery = require("../lib/index.js");
+
+vows.describe('Gamequery').addBatch({
+    // This basically tests for nothing
+    // This is done just to make sure that the package has no random errors that stop it from executing normally
+    "when querying a random Minecraft server": {
+        topic: function(){
+            gamequery.query({
+                type: 'minecraft',
+                host: 'mc.example.com'
+            }).then((state) => {
+                
+            }).catch((error) => {
+                
+            });
+
+            this.callback();
+        },
+        "we get an error" : (err)=>{
+            assert.isUndefined(err);
+        }
+    },
+}).export(module);
--- tests/index.js
+++
@@ -1,3 +0,0 @@
-const vows = require("vows");
-const assert = require("assert");
-const gamequery = require("../lib/index.js");
