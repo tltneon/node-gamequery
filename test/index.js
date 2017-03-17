73c306bd17da0b826b4389994cab54cd92b919da
Kurt Stolle <kurt@casualbananas.com>
2017-03-17 12:59:15 +0100

    Testing

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@ const vows = require("vows");
 const assert = require("assert");
 const gamequery = require("../lib/index.js");
 
+// Describe main module
 vows.describe('Gamequery').addBatch({
     // This basically tests for nothing
     // This is done just to make sure that the package has no random errors that stop it from executing normally
