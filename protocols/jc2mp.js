9a0f5a2c910ec78bdd580f977e3a79a774538351
Kurt Stolle <kurt@casualbananas.com>
2017-03-17 12:16:43 +0100

    +

--- protocols/jc2mp.js
+++ protocols/jc2mp.js
@@ -1,13 +1,12 @@
-
 module.exports = require('./gamespy3').extend({
-	init: function() {
+	init: function () {
 		this._super();
 		this.useOnlySingleSplit = true;
 	},
-	finalizeState: function(state) {
+	finalizeState: function (state) {
 		this._super(state);
-		if(!state.players.length && parseInt(state.raw.numplayers)) {
-			for(let i = 0; i < parseInt(state.raw.numplayers); i++) {
+		if (!state.players.length && parseInt(state.raw.numplayers)) {
+			for (let i = 0; i < parseInt(state.raw.numplayers); i++) {
 				state.players.push({});
 			}
 		}
